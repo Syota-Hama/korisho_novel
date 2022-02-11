2b81ff036aedecfd09a750b0b63f0f829a545e94
Syota-Hama <77922154+Syota-Hama@users.noreply.github.com>
2022-02-12 04:14:28 +0900

    ページテキストの保存機能

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,25 @@
 class PagesController < ApplicationController
+
   def index
     @pages = Page.all #find(params[:book_id]) books#newにてpageテーブルに最初のページを作成するフォームを実装後、記述を変更
+  end
+
+  def new
+    @page = Page.new
+    @book = Book.find(params[:book_id])
+  end
+
+  def create
+    page = Page.new(page_params)
+    if page.save
+      redirect_to book_pages_path
+    else
+      render new_book_page_path
+    end
+  end
+
+  private
+  def page_params
+    params.require(:page).permit(:page_title, :novel_page).merge(user_id: current_user.id, book_id: params[:book_id])
   end
 end
