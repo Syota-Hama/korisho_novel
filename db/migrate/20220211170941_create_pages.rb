229d1c427f1e2d95cbb4ac2e20cff1a8436b714f
Syota-Hama <77922154+Syota-Hama@users.noreply.github.com>
2022-02-12 02:14:54 +0900

    db:migrateの実行

--- db/migrate/20220211170941_create_pages.rb
+++ db/migrate/20220211170941_create_pages.rb
@@ -1,7 +1,10 @@
 class CreatePages < ActiveRecord::Migration[6.0]
   def change
     create_table :pages do |t|
-
+      t.string        :page_title,     null: false
+      t.text          :novel_page,     null: false
+      t.references    :user,           null: false, foreign_key: true
+      t.references    :book,           null: false, foreign_key: true
       t.timestamps
     end
   end
