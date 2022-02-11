dc2d772ba64278df6ad5c78ec5bdaf8cf5da8767
Syota-Hama <77922154+Syota-Hama@users.noreply.github.com>
2022-02-12 01:44:22 +0900

    小説投稿ぺージの実装

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,4 +2,22 @@ class BooksController < ApplicationController
   def index
     @books = Book.all
   end
+
+  def new
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to root_path
+    else
+      render new_book_path
+    end
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:novel_title, :synopsis, :genre_id).merge(user_id: current_user.id)
+  end
 end
